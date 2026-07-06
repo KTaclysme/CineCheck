a15f12213561cda172b4b43c0cc96c8c4591dc69
KTaclysme <takevin.pro@gmail.com>
2026-07-06 14:30:21 +0200

    auth(src/auth): add token

--- back/cinecheck/src/auth/auth.service.ts
+++ back/cinecheck/src/auth/auth.service.ts
@@ -1,16 +1,22 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private jwtService: JwtService
+  ) {}
 
   async signIn(email: string, pass: string): Promise<any> {
     const user = await this.usersService.findOne(email);
     if (user?.pword !== pass) {
       throw new UnauthorizedException();
     }
-    const { pword, ...result } = user;
-    return result;
+    const payload = { sub: user.id, email: user.email  };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
---
+++ back/cinecheck/src/auth/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+  secret: 'Nothing secret here',
+};
